3e0724c21c89f7be9d7c7cae08f481eaf76faada
Tiago André <tandre@moonlight.lan>
2016-01-07 16:06:33 +0000

    show errors ready for the views

--- errors.class.php
+++ errors.class.php
@@ -1,23 +1,11 @@
 <?php
-
   class error{
     private $errors;
-    function __construct(){
-
-    }
-    static function set($error){
-      echo $error;
-    }
-    public function add($name,$errormsg, $type){
-      $erro = new stdClass();
-      $erro->name = $name;
-      $erro->errormsg = $errormsg;
-      $erro->type = $type;
-      $this->errors[] = $erro;
-    }
     /**
-     * [show mostra erros guardados]
-     * @return [type] [description]
+     * show gera array de erros a fim de serem mostrados nas views
+     * @param  [string] $msg  [Texto a ser apresentado]
+     * @param  [string] $type [tipo de erro pode ser: danger, success, warning ou info]
+     * @return [Array]       [Array preparado para ser usado pela template engine e para as views  desta package]
      */
     static function show($msg, $type){
       return array("error" => $msg, "type" => $type);
